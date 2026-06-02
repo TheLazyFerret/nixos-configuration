e116b1a56d3eac52a6c3faa84bfd5187d6f73df6
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-02 22:33:33 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "System flake";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+  };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
