87881b4c300fef8625c46eaceac13ea3ed1c3ee5
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-21 19:16:02 +0100

    move desktop to subdirectory

--- modules/sound.nix
+++ modules/sound.nix
@@ -1,25 +1,25 @@
 ### Sound module.
 
 {pkgs, lib, config, ...}: {
-  
-  services.pulseaudio.enable = false; # Disable pulseaudio.
-  security.rtkit.enable = true; # Enable realtime scheduling on demand.
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   ### Variable declarations.
   options = {
-    cracking-fix.enable = lib.mkEnableOption "Enable some configurations for pipewire to reduce crackling sound";
+    crackingFix.enable = lib.mkEnableOption "Enable some configurations for pipewire to reduce crackling sound";
 
   };
 
   ### Assign values to the variables.
-  config = mkIf config.cracking-fix.enable {
-    services.pipewire.extraConfig.pipewire."10-cracking" = {
+  config = {
+    services.pulseaudio.enable = false; # Disable pulseaudio.
+    security.rtkit.enable = true; # Enable realtime scheduling on demand.
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    services.pipewire.extraConfig.pipewire."10-cracking" = lib.mkIf config.crackingFix.enable {
       "context.properties" = {
         "default.clock.rate" = 48000;
         "default.clock.allowed-rates" = [ 48000 ];
@@ -29,4 +29,5 @@
       };
     }; 
   };
+ 
 }
