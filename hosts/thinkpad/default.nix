454fdea5743e8243fe58f193ab07c309628161dc
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-28 23:49:35 +0100

    formatter

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -1,32 +1,32 @@
-### 
+###
 ### Thinkpad L14 gen 5 AMD
 { pkgs, ... }: {
-  
+
   # Imports:
   imports = [
     ./hardware-configuration.nix
   ];
 
   # Disable hibernation. At least in my tests, the laptop didn't support it.
-  customOptions.disableHibernation.enable = true; 
+  customOptions.disableHibernation.enable = true;
 
   # Keymap configuration.
   services.xserver.xkb.layout = "es";
   console.keyMap = "es";
 
   # Hostname.
-  networking.hostName = "thinkpad"; 
+  networking.hostName = "thinkpad";
 
   # Suspend fix for thinkpad L14 gen 5 (amd).
-  networking.networkmanager.wifi.powersave = false; 
+  networking.networkmanager.wifi.powersave = false;
 
   # Use systemd boot.
   boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  
+
   # Channel version when the system was installed.
   system.stateVersion = "26.05";
 }
