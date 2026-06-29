3923a8bc33416bc6b1bc3ef2c46ac1595e000742
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-29 20:29:44 +0100

    Change from weekly to daily nix-store optimise and garbage collector

--- modules/common/nix-store.nix
+++ modules/common/nix-store.nix
@@ -2,13 +2,13 @@
 { ... }: {
   nix.optimise = {
     automatic = true;
-    dates = [ "weekly" ];
+    dates = [ "daily" ];
     persistent = true; # https://github.com/NixOS/nixpkgs/blob/nixos-26.05/nixos/modules/services/misc/nix-optimise.nix
   };
 
   nix.gc = {
     automatic = true;
-    dates = "weekly";
+    dates = "daily";
     persistent = true;
   };
 }
