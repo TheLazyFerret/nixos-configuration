70191a79efce68bbaca508f837220a47fc71cf89
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-06-29 20:29:04 +0100

    Add unfree packages to desktop/packages

--- modules/desktop/packages.nix
+++ modules/desktop/packages.nix
@@ -1,18 +1,39 @@
 ### Packages a want in a desktop environmnent.
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    zed-editor
-    gimp
-    starship
-  ];
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.customOptions.unfreePackages;
+in
+{
+  options.customOptions.unfreePackages.enable = lib.mkIf "Enable extra unfree desktop packages";
+  
+  config = lib.mkMerge [
+    # Free software.
+    {
+      environment.systemPackages = with pkgs; [
+        zed-editor
+        gimp
+        starship
+      ];
 
-  fonts.packages = with pkgs; [
-    inconsolata
-    nerd-fonts.inconsolata
-  ];
+      fonts.packages = with pkgs; [
+        inconsolata
+        nerd-fonts.inconsolata
+      ];
 
-  programs.firefox = {
-    enable = true;
-    languagePacks = [ "es-ES" "en-US" ];
-  };
+      programs.firefox = {
+        enable = true;
+        languagePacks = [
+          "es-ES"
+          "en-US"
+        ];
+      };
+    }
+    # Unfree software.
+    (lib.mkIf (cfg.enable) {
+      environment.systemPackages =  with pkgs; [
+        discord
+        telegram-desktop
+      ];
+    })
+  ];
 }
