b2a8f7d544cc5d2a1bbf1477b61dc1df205c2168
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-07-12 14:56:33 +0100

    Fix suspend

--- hosts/r5600/default.nix
+++ hosts/r5600/default.nix
@@ -12,7 +12,7 @@
   # Gaming options.
   customOptions.steam.enable = true;
   customOptions.prismlauncher.enable = true;
-  
+
   # Disable hibernation.
   customOptions.disableHibernation.enable = true;
 
@@ -22,7 +22,7 @@
   # Keymap configuration.
   services.xserver.xkb.layout = "us";
   services.xserver.xkb.variant = "altgr-intl";
-  console.keyMap = "en"; 
+  console.keyMap = "en";
 
   # Hostname.
   networking.hostName = "r5600";
@@ -33,6 +33,12 @@
 
   # Kernel to use.
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Fix workaround for board B550 AORUS ELITE V2
+  # See: https://wiki.archlinux.org/title/Power_management/Wakeup_triggers#Instantaneous_wakeup_after_suspending
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"
+  '';
 
   # Channel version when the system was installed.
   system.stateVersion = "26.05";
